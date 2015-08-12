4a81d266514b1708a5eb8e00760f0d5d1d0a74e0
Juan Palacios <juan.palacios@epfl.ch>
2015-08-12 17:58:49 +0200

    Create DB tables. Clean up man python file.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,14 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE DATABASE tournament;
 
+\c tournament;
+
+CREATE TABLE players (id SERIAL PRIMARY KEY,
+                      name VARCHAR(128) NOT NULL);
+
+CREATE TABLE matches (id SERIAL PRIMARY KEY,
+                      player_a_id INT REFERENCES players(id),
+                      player_b_id INT REFERENCES players(id),
+                      winner_id INT REFERENCES players(id));
